9185b0513a813adf2f2cadf3dfa69e7ef5f93ff7
LyalinDima <lyalindim@gmail.com>
2022-05-22 20:34:14 +0500

    First commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,36 @@
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        CloseableHttpClient httpClient = HttpClientBuilder.create()
+                .setDefaultRequestConfig(RequestConfig.custom()
+                        .setConnectTimeout(5000)
+                        .setSocketTimeout(30000)
+                        .setRedirectsEnabled(false)
+                        .build())
+                .build();
+        HttpGet request = new HttpGet("https://api.nasa.gov/planetary/apod?api_key=mryFjiWJtcKAY3KMncxu8qzabLd0EuuTedSzJWVx");
+        CloseableHttpResponse response = httpClient.execute(request);
+        String body = new String(response.getEntity().getContent().readAllBytes());
+        System.out.println(body);
+        GsonBuilder builder = new GsonBuilder();
+        Gson gson = builder.create();
+        NASAAnswer nasaAnswer = gson.fromJson(body, NASAAnswer.class);
+        System.out.println(nasaAnswer);
+        String fileURL = nasaAnswer.getUrl();
+        request = new HttpGet(fileURL);
+        response = httpClient.execute(request);
+        byte[] image = response.getEntity().getContent().readAllBytes();
+        FileOutputStream fileOutputStream = new FileOutputStream(fileURL.substring(fileURL.lastIndexOf("/") + 1));
+        fileOutputStream.write(image, 0, image.length);
+    }
+}
---
+++ src/main/java/NASAAnswer.java
@@ -0,0 +1,61 @@
+public class NASAAnswer {
+    private String date;
+    private String explanation;
+    private String hdurl;
+    private String media_type;
+    private String service_version;
+    private String title;
+    private String url;
+
+    public NASAAnswer(String date, String explanation, String hdurl, String media_type, String service_version, String title, String url) {
+        this.date = date;
+        this.explanation = explanation;
+        this.hdurl = hdurl;
+        this.media_type = media_type;
+        this.service_version = service_version;
+        this.title = title;
+        this.url = url;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getExplanation() {
+        return explanation;
+    }
+
+    public String getHdurl() {
+        return hdurl;
+    }
+
+    public String getMedia_type() {
+        return media_type;
+    }
+
+    public String getService_version() {
+        return service_version;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    @Override
+    public String toString() {
+        return "NASAAnswer{" +
+                ", date='" + date + '\'' +
+                ", explanation='" + explanation + '\'' +
+                ", hdurl='" + hdurl + '\'' +
+                ", media_type='" + media_type + '\'' +
+                ", service_version='" + service_version + '\'' +
+                ", title='" + title + '\'' +
+                ", url='" + url + '\'' +
+                '}';
+    }
+}
+
