f5f1756b7fe3cf57265089210761cf82024c1ccd
LyalinDima <lyalindim@gmail.com>
2022-05-24 20:12:03 +0500

    Add try catch

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,27 +10,31 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        CloseableHttpClient httpClient = HttpClientBuilder.create()
+    public static void main(String[] args) {
+        try (CloseableHttpClient httpClient = HttpClientBuilder.create()
                 .setDefaultRequestConfig(RequestConfig.custom()
                         .setConnectTimeout(5000)
                         .setSocketTimeout(30000)
                         .setRedirectsEnabled(false)
                         .build())
-                .build();
-        HttpGet request = new HttpGet("https://api.nasa.gov/planetary/apod?api_key=mryFjiWJtcKAY3KMncxu8qzabLd0EuuTedSzJWVx");
-        CloseableHttpResponse response = httpClient.execute(request);
-        String body = new String(response.getEntity().getContent().readAllBytes());
-        System.out.println(body);
-        GsonBuilder builder = new GsonBuilder();
-        Gson gson = builder.create();
-        NASAAnswer nasaAnswer = gson.fromJson(body, NASAAnswer.class);
-        System.out.println(nasaAnswer);
-        String fileURL = nasaAnswer.getUrl();
-        request = new HttpGet(fileURL);
-        response = httpClient.execute(request);
-        byte[] image = response.getEntity().getContent().readAllBytes();
-        FileOutputStream fileOutputStream = new FileOutputStream(fileURL.substring(fileURL.lastIndexOf("/") + 1));
-        fileOutputStream.write(image, 0, image.length);
+                .build()
+
+        ) {
+            HttpGet request = new HttpGet("https://api.nasa.gov/planetary/apod?api_key=mryFjiWJtcKAY3KMncxu8qzabLd0EuuTedSzJWVx");
+            CloseableHttpResponse response = httpClient.execute(request);
+            String body = new String(response.getEntity().getContent().readAllBytes());
+            GsonBuilder builder = new GsonBuilder();
+            Gson gson = builder.create();
+            NASAAnswer nasaAnswer = gson.fromJson(body, NASAAnswer.class);
+            System.out.println(nasaAnswer);
+            String fileURL = nasaAnswer.getUrl();
+            request = new HttpGet(fileURL);
+            response = httpClient.execute(request);
+            byte[] image = response.getEntity().getContent().readAllBytes();
+            FileOutputStream fileOutputStream = new FileOutputStream(fileURL.substring(fileURL.lastIndexOf("/") + 1));
+            fileOutputStream.write(image, 0, image.length);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
